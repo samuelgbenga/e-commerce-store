c95c6460e2ce73dc17ba865f8f7d2c3305471abf
samuelgbenga <samuelgbenga972@gmail.com>
2021-09-20 04:58:41 +0100

    changed the primary and secondary color of the product component

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,28 +1,42 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, createTheme, ThemeProvider } from "@material-ui/core";
+import { green, blue } from "@material-ui/core/colors";
 import Product from "./product/Product";
 import useStyle from "./product/style";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[500],
+    },
+    secondary: {
+      main: green[200],
+    },
+  },
+});
 
 const Products = ({ products, handleAddToCart }) => {
   const classes = useStyle();
   return (
-    <main className={classes.productsRoot}>
-      <Grid container justifyContent="center" spacing={4}>
-        {products.map((product) => (
-          <Grid
-            style={{ textAlign: "center" }}
-            item
-            key={product.id}
-            xs={12}
-            sm={6}
-            md={4}
-            lg={3}
-          >
-            <Product product={product} handleAddToCart={handleAddToCart} />
-          </Grid>
-        ))}
-      </Grid>
-    </main>
+    <ThemeProvider theme={theme}>
+      <main className={classes.productsRoot}>
+        <Grid container justifyContent="center" spacing={4}>
+          {products.map((product) => (
+            <Grid
+              style={{ textAlign: "center" }}
+              item
+              key={product.id}
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+            >
+              <Product product={product} handleAddToCart={handleAddToCart} />
+            </Grid>
+          ))}
+        </Grid>
+      </main>
+    </ThemeProvider>
   );
 };
 
